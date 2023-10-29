b516a2358c51216ef518c7b9a7db67985eb4ab5f
Ayush05m <72142971+Ayush05m@users.noreply.github.com>
2023-10-29 22:50:51 +0530

    Update cardIcrement.js

--- ICell/Incrementation/cardIcrement.js
+++ ICell/Incrementation/cardIcrement.js
@@ -2,12 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const numberElement = document.querySelectorAll('.numCategory');
     const targetElement = document.querySelector('.card');
     const targetNumber = [1000, 100, 2000, 1000];    // final values for each card element
-    const incrementInterval = 100;      // increment interval in milliseconds
-
+    const incrementInterval = 40;      // increment interval in milliseconds
+    const animationDuration = 2000;
     const processNextElement = (i) => {
         let tn = targetNumber[i];
-        let currentNumber = targetNumber[i] / 2;
+        let currentNumber = targetNumber[i] / 5 * 2;
         let halfwayNumber = (targetNumber[i]) * 9 / 10;
+        const incrementStep = Math.floor((targetNumber[i] - currentNumber) / (animationDuration / incrementInterval));
+
 
         const changeContent = () => {
             if (currentNumber < targetNumber[i]) {
@@ -16,21 +18,20 @@ document.addEventListener('DOMContentLoaded', () => {
             }
             else {
                 numberElement[i].innerHTML = targetNumber[i] + ' + ';
-                // setTimeout(increment, incrementInterval);
             }
         };
 
         const increment = () => {
             if (currentNumber > halfwayNumber && (currentNumber < targetNumber[i])) {
-                currentNumber += 11;
+                currentNumber += incrementStep;
                 changeContent();
             }
             else if (currentNumber < targetNumber[i] && targetNumber[i] >= 1000) {
-                currentNumber += 111;
+                currentNumber += incrementStep;
                 changeContent(currentNumber, i);
             }
             else if (currentNumber < targetNumber[i] && targetNumber[i] < 1000) {
-                currentNumber += 11;
+                currentNumber += incrementStep + 1;
                 changeContent(currentNumber, i);
             }
         };
@@ -42,20 +43,18 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
 
-    const options = {
-        root: null, // Use the viewport as the root
-        rootMargin: '0px', // No margin
-        threshold: 0.5, // When 50% of the element is visible, trigger the callback
-    };
+    function respondToVisibility(element, callback) {
+        var options = {
+            root: document.documentElement,
+        };
 
-    // Create the Intersection Observer. This is copied from the chatGpt
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                observer.disconnect();
-            }
-        });
-    }, options);
-    observer.observe(targetElement);
+        var observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                callback(entry.intersectionRatio > 0);
+            });
+        }, options);
+
+        observer.observe(element);
+    }
 
 });
