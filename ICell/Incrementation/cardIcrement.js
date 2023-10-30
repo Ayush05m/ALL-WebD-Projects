30a18aaffdb2a58523dff3e250278c9494069706
Ayush05m <72142971+Ayush05m@users.noreply.github.com>
2023-10-30 16:13:50 +0530

    Update cardIcrement.js

--- ICell/Incrementation/cardIcrement.js
+++ ICell/Incrementation/cardIcrement.js
@@ -1,60 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
     const numberElement = document.querySelectorAll('.numCategory');
     const targetElement = document.querySelector('.card');
+
     const targetNumber = [1000, 100, 2000, 1000];    // final values for each card element
-    const incrementInterval = 40;      // increment interval in milliseconds
-    const animationDuration = 2000;
+    let incrementInterval = 40;      // increment interval in milliseconds
+    const animationDuration = 1500; // animation duration in milliseconds
+
     const processNextElement = (i) => {
         let tn = targetNumber[i];
         let currentNumber = targetNumber[i] / 5 * 2;
         let halfwayNumber = (targetNumber[i]) * 9 / 10;
-        const incrementStep = Math.floor((targetNumber[i] - currentNumber) / (animationDuration / incrementInterval));
+        const incrementStep = Math.floor((targetNumber[i] /2) / (animationDuration / incrementInterval)); //calculate increment to end the increment at the time
 
-
+        // This fuction updates the html element
         const changeContent = () => {
             if (currentNumber < targetNumber[i]) {
-                numberElement[i].innerHTML = currentNumber;
+                numberElement[i].innerHTML = currentNumber; //update when the current number is less than the target number
                 setTimeout(increment, incrementInterval);
             }
             else {
-                numberElement[i].innerHTML = targetNumber[i] + ' + ';
+                numberElement[i].innerHTML = targetNumber[i] + ' + '; //update when the current number is greater than or equal to the target number
             }
         };
 
+        // Function decides when and how much to increment 
         const increment = () => {
-            if (currentNumber > halfwayNumber && (currentNumber < targetNumber[i])) {
-                currentNumber += incrementStep;
+            if (currentNumber > halfwayNumber && (currentNumber < 9*targetNumber[i]/10)) {
+                currentNumber += 11;
+                changeContent(); //calling changeContent to update html
+            }
+            else if (currentNumber > 9*targetNumber[i]/10 ) {
+                currentNumber += 1;
+
+                // Change the increment interval according to the value size after 90% completed
+                if (targetElement[i] <500){
+                    incrementInterval = 10;
+                }
+                else{
+                    incrementInterval = 7;
+                }
                 changeContent();
             }
-            else if (currentNumber < targetNumber[i] && targetNumber[i] >= 1000) {
+            else if (currentNumber < targetNumber[i]) {
                 currentNumber += incrementStep;
-                changeContent(currentNumber, i);
-            }
-            else if (currentNumber < targetNumber[i] && targetNumber[i] < 1000) {
-                currentNumber += incrementStep + 1;
-                changeContent(currentNumber, i);
+                changeContent();
             }
         };
 
         setTimeout(increment, incrementInterval);
     };
+
+    //To run the function of increment for the no. of cards
     for (let i = 0; i < targetNumber.length; i++) {
         processNextElement(i);
-    };
-
-
-    function respondToVisibility(element, callback) {
-        var options = {
-            root: document.documentElement,
-        };
-
-        var observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                callback(entry.intersectionRatio > 0);
-            });
-        }, options);
-
-        observer.observe(element);
     }
 
 });
